c6300adf049456e2c093083e7d140bb5d23eb873
Ugeon <dnrjs422@gmail.com>
2024-06-07 01:11:43 +0900

    20240607

--- js/script.js
+++ js/script.js
@@ -50,20 +50,6 @@ const movingItem = {
     left: 0,
 };
 
-const soundFiles = [
-    "effect/move.mp3",
-    "effect/drop.mp3",
-    "effect/nothold.mp3",
-    "effect/matched.mp3",
-    "effect/gameover.mp3",
-    "effect/hold.mp3",
-    "effect/rotate.mp3",
-    "music/theme1.mp3",
-    "music/theme2.mp3",
-    "music/theme3.mp3",
-    "music/theme4.mp3"
-];
-
 let effectVolume = document.getElementById('effectVolumeSlider').value / 100;
 
 // 사운드 볼륨을 설정하는 함수
